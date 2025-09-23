bb4eeb5541414d793874023ae7b1c602e7b9ab10
bjgeumgang <bjgeumgang@gmail.com>
2025-09-23 09:48:21 +0900

    useMemo add

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './List.css';
 import TodoItem from './TodoItem';
 
@@ -20,9 +20,28 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilterData();
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // 의존성배열 : deps
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
