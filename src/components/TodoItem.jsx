6470fb6fa6a0d1e2260f6f308a40d39ee09c528c
bjgeumgang <bjgeumgang@gmail.com>
2025-09-25 22:03:02 +0900

    useCallback_rerender

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -25,13 +25,15 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
     </div>
   );
 };
-export default memo(TodoItem, (prevProps, nextProps) => {
-  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단함
-  // T -> Props 바뀌지 않음 -> 리렌더링 X
-  // F -> Props 바뀜 -> 리렌더링 O
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
-  return true;
-});
+//고차 컴포넌트 (HOC)
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단함
+//   // T -> Props 바뀌지 않음 -> 리렌더링 X
+//   // F -> Props 바뀜 -> 리렌더링 O
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+//   return true;
+// });
+export default memo(TodoItem);
